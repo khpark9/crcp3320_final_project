51a008f63fe7d0febd09e4ed16f8498a6105192b
khpark9 <105660250+khpark9@users.noreply.github.com>
2024-11-24 17:34:53 -0600

    waves.js file to start making sine and cosine waves

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,8 +1,31 @@
+// let numWaves = 5;
+let numParticles = Math.floor(random(50, 100));
+let size = 4;
+let amp = Math.floor(random(25, 50));
+let freq = 0.02;
+let speed = 2;
+let position;
+
+let wave;
+let particles = [];
+
 function setup() {
     createCanvas(500, 500);
+
+    position = createVector(0, height/2);
+    
+    for (let i = 0; i < numParticles; i++) {
+        wave.push({
+            offset: i * 20,
+            phase: Math.random() * Math.PI * 2, // Random phase
+            color: `hsl(${(i / numWaves) * 360}, 80%, 60%)`, 
+        });
+    }
 }
+    
 
 function draw() {
     background(0);
+
 }
 
