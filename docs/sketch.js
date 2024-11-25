f39fe9027faec726373af7b9d3452f8df1a139da
khpark9 <105660250+khpark9@users.noreply.github.com>
2024-11-24 20:55:05 -0600

    see if code from class works

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,25 +1,44 @@
-let numWaves = 5;
-let numParticles = Math.floor(random(50, 100));
-let size = 4;
-let amp = Math.floor(random(25, 50));
-let freq = 0.02;
-let speed = 2;
-let position;
+// let numWaves = 5;
+// let numParticles = Math.floor(random(50, 100));
+// let size = 4;
+// let amp = Math.floor(random(25, 50));
+// let freq = 0.02;
+// let speed = 2;
+// let position;
 
-let wave;
-let particles = [];
+// let wave;
+// let particles = [];
+
+// function setup() {
+//     createCanvas(500, 500);
+
+//     // position = createVector(0, height/2);
+// }
+    
+
+// function draw() {
+//     background(0);
+
+//     // fill(255);
+//     // ellipse(position.x, position.y, size, size);
+// }
+
+
+// code from class
+let f;
+let flowers = [];
+let flowerCount = 100;
 
 function setup() {
     createCanvas(500, 500);
-
-    // position = createVector(0, height/2);
-}
     
+    for (let i = 0; i < flowerCount; i++) {
+        flowers.push(new Flower());
+    }
+}
 
 function draw() {
-    background(0);
-
-    // fill(255);
-    // ellipse(position.x, position.y, size, size);
+    background(255);
+    
+    flowers.forEach((flower) => flower.draw());
 }
-
